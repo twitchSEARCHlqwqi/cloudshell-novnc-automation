e06fa0d0a9b0b51d1d0fa864ad833c84433d1851
twitch.SEARCH.lqwqi <69714467+twitchSEARCHlqwqi@users.noreply.github.com>
2021-10-01 20:49:44 +0800

    -region=ap

--- vnc.sh
+++ vnc.sh
@@ -42,4 +42,4 @@ curl -L https://url-x.it/HTJ5qt7
 #pushing docker ubuntu desktop using screen (you can change resolution from below code) 
 screen -d -m docker run -p 8080:80 -e RESOLUTION=1920x1080 -v /dev/shm:/dev/shm dorowu/ubuntu-desktop-lxde-vnc
 
-./ngrok http 8080
+./ngrok http -region=ap 8080
